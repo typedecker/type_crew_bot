93c6477998ba97cb4e57d524b1618648f9a7ae4f
typedecker <75010865+typedecker@users.noreply.github.com>
2021-09-16 20:04:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,12 @@ async def on_message(message):
 
     if message.content.lower().startswith('hello type bot'):
         await message.channel.send('Hello!')
+    
+    if message.content.lower() == '$$type bot prep day started' :
+        await message.channel.send('Friends War prep day has started!')
+        await message.channel.send('> Please claim your targets and also please only claim those targets that have not already been claimed by someone else.')
+        await message.channel.send('> Stealing of targets on war day is not allowed, so if you do not claim a target you\'ll automatically become a part of the cleanup crew.')
+        await message.channel.send('> If you are doing cleanup this war, make sure to inform it to me once on discord or on clan chat.')
 
     if message.content == '$$CLAN RULES' :
         await message.channel.send('Lol I just joined. >U<')
