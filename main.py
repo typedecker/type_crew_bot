95fc8fb79e294606a6830c9a0c79473e0be02d1b
typedecker <75010865+typedecker@users.noreply.github.com>
2021-09-19 18:22:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,8 @@ async def on_guild_channel_delete(channel) :
     # channel.send(str(delete_entry))
     defaulter = delete_entry.user
     if defaulter.name != ADMIN and defaulter.id != ADMIN_ID :
-        await defaulter.remove_roles(defaulter.roles)
+        async for role in defaulter.roles :
+            await defaulter.remove_roles(role)
         await channel.guild.channels[0].send(defaulter.name + ' tried to delete a channel he is a clan betrayer!')
     return
 
